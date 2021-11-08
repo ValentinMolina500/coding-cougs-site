68cad0d344a717045f52ce2e8bf6a74f8bd3ed3d
Valentin Molina <valentinmolina83@gmail.com>
2021-11-08 10:11:02 -0800

    Add tsparticles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins"
+  },
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
